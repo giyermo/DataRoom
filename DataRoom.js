93122dad97611b647a4fb79afd72a8181d7e497a
Guillermo <guillermo@trastazo.mi.casa>
2020-06-19 22:22:21 +0200

    charts inside podiums change

--- DataRoom.js
+++ DataRoom.js
@@ -1,70 +1,24 @@
-// Code for a component implementing texture coming from a camera
-//
-// Original code:
-//   https://wirewhiz.com/how-to-use-a-cameras-output-as-a-texture-in-aframe/
-//
-AFRAME.registerComponent('camrender',{
-     'schema': {
-        // desired FPS
-        fps: {
-             type: 'number',
-             default: 90.0
-        },
-        // Id of the canvas element used for rendering the camera
-        cid: {
-             type: 'string',
-             default: 'camRenderer'
-        },
-        // Height of the renderer element
-        height: {
-             type: 'number',
-             default: 300
-        },
-        // Width of the renderer element
-        width: {
-             type: 'number',
-             default: 400
-        }
-     },
-     'update': function(oldData) {
-         var data = this.data
-         if (oldData.cid !== data.cid) {
-             // Find canvas element to be used for rendering
-             var canvasEl = document.getElementById(this.data.cid);
-             // Create renderer
-             this.renderer = new THREE.WebGLRenderer({
-                 antialias: true,
-                 canvas: canvasEl
-             });
-             // Set properties for renderer DOM element
-             this.renderer.setPixelRatio( window.devicePixelRatio );
-             this.renderer.domElement.crossorigin = "anonymous";
-         };
-         if (oldData.width !== data.width || oldData.height !== data.height) {
-             // Set size of canvas renderer
-             this.renderer.setSize(data.width, data.height);
-             this.renderer.domElement.height = data.height;
-             this.renderer.domElement.width = data.width;
-         };
-         if (oldData.fps !== data.fps) {
-             // Set how often to call tick
-             this.tick = AFRAME.utils.throttleTick(this.tick, 1000 / data.fps , this);
-         };
-     },
-     'tick': function(time, timeDelta) {
-         this.renderer.render( this.el.sceneEl.object3D , this.el.object3DMap.camera );
-     }
- });
- 
- AFRAME.registerComponent('canvas-updater', {
-     dependencies: ['geometry', 'material'],
- 
-     tick: function () {
-          var el = this.el;
-          var material;
- 
-          material = el.getObject3D('mesh').material;
-          if (!material.map) { return; }
-         material.map.needsUpdate = true;
-     }
- });
+AFRAME.registerComponent('info-podium', {
+    schema: {
+        chartType: {type: 'string', default: ''},
+    },
+
+    init: function() {
+        let data = this.data;
+        let el = this.el;  
+
+        let scene = document.getElementById(scene);
+
+        let podium = document.createElement('a-box');
+
+        let chart = document.createElement('a-entity');
+
+        scene.appendChild(podium);
+
+        podium.setAttribute('scale', {x: 5.5, y: 1, z: 5.5});
+        
+        scene.appendChild(chart);
+
+        chart.setAttribute();
+    }
+});
