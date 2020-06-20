d5125f1fe67bc97d97e8d00a1d6374c171552f48
Guillermo <guillermo@trastazo.mi.casa>
2020-06-20 16:37:22 +0200

    time changer functionality

--- DataRoom.js
+++ DataRoom.js
@@ -1,3 +1,5 @@
+let time = 'night';
+
 AFRAME.registerComponent('info-podium', {
     schema: {
         podiumWidth: {type: 'string', default: '5.5'},
@@ -27,5 +29,32 @@ AFRAME.registerComponent('info-podium', {
         podium.setAttribute('position', data.podiumPosition);
 
         podium.setAttribute('static-body', '');
+    }
+});
+
+AFRAME.registerComponent('time-changer', {
+    init: function() {
+        let data = this.data;
+        let el = this.el;  
+
+        let sky = document.getElementById('sky');
+
+        let roomLight = document.getElementById('roomLight');
+
+        el.addEventListener('ybuttondown', function(evt) {
+            if(time == 'night') {
+                sky.setAttribute('src', '#daySky');
+
+                roomLight.setAttribute('color', '#EEF66C');
+
+                time = 'day';
+            } else {
+                sky.setAttribute('src', '#nightSky');
+
+                roomLight.setAttribute('color', 'lightgrey');
+
+                time = 'night';
+            };
+        });
     }
 });
